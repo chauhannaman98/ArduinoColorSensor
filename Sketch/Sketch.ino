d529761b25d62747915629db6da63b772fcd6b6e
Naman Chauhan <chauhannaman98@gmail.com>
2018-08-04 16:14:14 +0530

    sketch commit

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -5,23 +5,76 @@
  *
  * August 18, 2017 © GPL3+
  * Author : Naman Chauhan
- * 
- ********************************************************************************
- *   
- * The circuit:
- * * VCC connection of the sensor attached to +5V
- * * GND connection of the sensor attached to ground
- * * TRIG connection of the sensor attached to digital pin 
- * * ECHO connection of the sensor attached to digital pin 
     
 ********************************************************************************/
 
+//RGB pin declarations
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 6;
+
+//color sensor pin declarations
+int S2 = 7; //color sensor pin S2 to Arduino pin 7
+int S3 = 8; //color sensor pin S3 to Arduino pin 8
+int outPin = 4;   //color sensor out to Arduino pin 4
+
+unsigned int pulseWidth;
+
+int rColorStrength;
+int gColorStrength;
+int bColorStrength;
+
 void setup() {
   // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  
+  pinMode(redPin,OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
 
+  pinMode(S2,OUTPUT);
+  pinMode(S3,OUTPUT);
+  pinMode(outPin,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //reading red component of color
+  //S2 and S3 should be set low
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
 
+  pulseWidth = pulseIn(outPin,LOW);
+
+  rColorStrength = pulseWidth/400. -1;
+  rColorStrength = (255 - rColorStrength);
+
+
+  //reading green component of color
+  //S2 and S3 should be set high
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+
+  pulseWidth = pulseIn(outPin,LOW);
+
+  gColorStrength = pulseWidth/400. -1;
+  gColorStrength = (255 - gColorStrength);
+
+
+  //reading blue component of color
+  //S2 should be high and S3 to be high
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+
+  pulseWidth = pulseIn(outPin,LOW);
+
+  bColorStrength = pulseWidth/400. -1;
+  bColorStrength = (255 - bColorStrength);
+
+  Serial.print(rColorStrength);
+  Serial.print("  , ");
+  Serial.print(gColorStrength);
+  Serial.print("  , ");
+  Serial.println(bColorSterngth);
+  Serial.println("");
 }
