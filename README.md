dc9265add5e59c810e3a0f0467e2e8febb97400c
Naman Chauhan <chauhannaman98@gmail.com>
2018-08-04 15:38:03 +0530

    Commit changes to README and skecth.ino

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ArduinoColorSensor
 
-## This repo contains code for the color sensor.
+This repo contains code for the color sensor.
+
+## Connections:
+
+| Color Sensor Pin | Arduino Pin |
+| ---------------- | ----------- |
+| S0 | GND |
+| S1 | 5V |
+| S2 | pin 7 |
+| S3 | pin 8 |
+| OUT | pin 4 |
+| VCC | 5V |
+| GND | GND |
--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,5 +1,5 @@
 /*
- * https://github.com/chauhannaman98/Arduino-Car-Parking-Assistant
+ * https://github.com/chauhannaman98
  *
  * Source_Code.ino
  *
@@ -7,12 +7,6 @@
  * Author : Naman Chauhan
  * 
  ********************************************************************************
- *  
- * This sketch reads a HC-SR04 ultrasonic rangefinder and returns the
- * distance to the closest object in range. To do this, it sends a pulse
- * to the sensor to initiate a reading, then listens for a pulse 
- * to return.  The length of the returning pulse is proportional to 
- * the distance of the object from the sensor.
  *   
  * The circuit:
  * * VCC connection of the sensor attached to +5V
